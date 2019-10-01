240062e7b6780b21a4120cd517edd0fbb1f90ba8
Faisal <faisalpratamaputra@gmail.com>
2019-10-02 00:12:52 +0700

    raspi adjustment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const {app, BrowserWindow} = require('electron')
+const escpos = require('escpos')
+const fs = require('fs')
 
+const device  = new escpos.USB()
+const printer = new escpos.Printer(device)
 app.on('ready',()=>{
     const window = new BrowserWindow({
         width: 500,
@@ -11,3 +15,22 @@ app.on('ready',()=>{
     })
     window.loadFile('./html/index.html')
 })
+
+exports.print = (arrayBuffer)=>{
+    fs.writeFile("./hello.png", Buffer.from(arrayBuffer), err=>{
+        console.log(err)
+    })
+}
+/*
+escpos.Image.load('FRAME.png', 'image/png', image=>{
+    
+    console.log(image)
+    device.open(()=>{
+        printer.align('ct')
+        printer.text('image')
+        printer.raster(image, 'dwdh')
+        printer.cut()
+        printer.close()
+    })
+})
+*/
